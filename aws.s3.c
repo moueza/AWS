36b2d3eab38d75e2e3f1bf2713d762ee590c7da9
moueza <221372+moueza@users.noreply.github.com>
2020-05-08 22:21:08 +0200

    Update aws.s3.c

--- aws.s3.c
+++ aws.s3.c
@@ -1,7 +1,8 @@
-/*.c for comments highlight
+/*.c for comments highlight by editors
 https://console.aws.amazon.com/console/home?region=us-east-1
 https://docs.aws.amazon.com/AmazonS3/latest/dev/delete-or-empty-bucket.html#delete-bucket-awscli
-*/
+TODO no loop
+alternative to bash : sed awk*/
 aws s3 rb s3://buck --force
 //KO xargsaws s3 ls |grep 2019|xargs cut -c 2-1000
 //KO   aws s3 ls |cut -c 20-1000 |xargs aws s3 rb s3://'{}'
