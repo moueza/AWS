e3604bd1b650e6c5c2ab13e33c0f7da165ba45b7
moueza <221372+moueza@users.noreply.github.com>
2020-05-08 20:19:39 +0200

    Create aws.s3.c

---
+++ aws.s3.c
@@ -0,0 +1,4 @@
+/*.c for comments highlight
+https://console.aws.amazon.com/console/home?region=us-east-1
+https://docs.aws.amazon.com/AmazonS3/latest/dev/delete-or-empty-bucket.html#delete-bucket-awscli
+*/
